b623b42b98a4decb0d6475b64e7e498b153e2619
Merve <mervedemirel41@hotmail.com>
2023-10-10 22:26:06 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,24 +10,6 @@ const publicRuntimeConfig = {
         : 'none',
 };
 
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${ergida-old}/`
-  basePath = `/${ergida-old}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
-
 const nextConfig = {
   output: 'export',
 };
